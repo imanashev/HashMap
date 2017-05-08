28c42148d9406230b5d5925e2bf3d6f98451e593
Ildar Manashev <ildar404@gmail.com>
2017-05-08 23:57:30 +0700

    It works!

--- HashMap/HashMap.h
+++ HashMap/HashMap.h
@@ -3,31 +3,91 @@
 #include <iostream>
 #include <exception>
 
+#define SIZE_KOEF 1.4 //How much reserv
+#define MAX_LOAD_FACTOR 5 //max load factor for rehash
+
 template <class K, class V>
 class HashMap
 {
 private:
-	std::list<V>* table_;
+	std::list<std::pair<K,V>>* table_;
 	int n_; //size
-	int l_; //max lenght of all lists
+	int l_; //max lenght of all lists - load factor
+	void rehash();
 public:
 	HashMap(int n);
+
 	void insert(K& key, V& value);
 	bool remove(K& key);
-	V& operator[](K&);
+	V& operator[](K& key);
+	int getLoad();
 };
 
 
 template<class K, class V>
 inline HashMap<K, V>::HashMap(int n)
 {
-	n_ = (int)(n * 1.4);
-	table_ = new std::list<V>[n_];
+	n_ = (int)(n * SIZE_KOEF);
+	table_ = new std::list<std::pair<K, V>>[n_];
+	l_ = 0;
 }
 
 template<class K, class V>
 void HashMap<K, V>::insert(K & key, V & value)
 {
 	int hash = std::hash<K>()(key) % n_;
-	table_[hash].insert
+	table_[hash].push_front(std::make_pair(key, value));
+	if (table_[hash].size() > l_)
+	{
+		l_ = (int)table_[hash].size();
+		if (l_ > MAX_LOAD_FACTOR)
+		{
+			rehash();
+		}
+	}
+
+	
+}
+
+template<class K, class V>
+bool HashMap<K, V>::remove(K & key)
+{
+	int hash = std::hash<K>()(key) % n_;
+
+	for (auto it = table_[hash].begin(); it != table_[hash].end(); ++it)
+	{
+		if (it->first == key)
+		{
+			//delete
+			table_[hash].erase(it);
+			return true;
+		}
+	}
+
+	return false;
+}
+
+template<class K, class V>
+V & HashMap<K, V>::operator[](K & key)
+{
+	int hash = std::hash<K>()(key) % n_;
+
+	for (auto it = table_[hash].begin(); it != table_[hash].end(); ++it)
+	{
+		if (it->first == key)
+		{
+			return it->second;
+		}
+	}
+
+	std::cout << "Not found" << std::endl;
+	//TODO
+	//return it->second;
+
+}
+
+template<class K, class V>
+inline int HashMap<K, V>::getLoad()
+{
+	return l_;
 }
--- HashMap/Source.cpp
+++ HashMap/Source.cpp
@@ -2,7 +2,32 @@
 #include "HashMap.h"
 #include <string>
 
+using namespace std;
 void main()
 {
-	HashMap<std::string,std::string> dictionary(1000);
+	HashMap<std::string,std::string> dict(1000);
+
+	for (int i = 0; i < 1000; ++i)
+	{
+		std::string a, b;
+		a = "key" + std::to_string(i);
+		b = "value" + std::to_string(i);
+		dict.insert(a, b);
+	}
+
+	cout << "Load factor:" << dict.getLoad() << endl;
+	//std::string input;
+	//while (getline(std::cin, input))
+	//{
+	//	cout << dict[input] << endl;
+	//}
+	
+	for (int i = 0; i < 1000; ++i)
+	{
+		std::string a;
+		a = "key" + std::to_string(i);
+		cout << dict[a] << endl;
+		dict.remove(a);
+	}
+	cout << "Load factor:" << dict.getLoad() << endl;
 }
